1a7c9475b46a26c7aa9f4b725f05d3d66ee34d7d
QuangTrung96 <trinhquangtrung646@gmail.com>
2018-07-03 00:54:50 +0700

    [practice_with_session]

--- m2_practice_11/app/code/Demo/MultiAction/Block/Hello.php
+++ m2_practice_11/app/code/Demo/MultiAction/Block/Hello.php
@@ -2,18 +2,28 @@
 
 namespace Demo\MultiAction\Block;
 
-use Magento\Framework\View\Element\Template;
 use Magento\Framework\Registry;
+use Magento\Framework\View\Element\Template;
 
 class Hello extends Template
 {
     protected $_coreRegistry;
+    protected $_catalogSession;
+    protected $_customerSession;
+    protected $_checkoutSession;
 
-    public function __construct(Template\Context $context, 
+    public function __construct(Template\Context $context,
+                                \Magento\Catalog\Model\Session $catalogSession,
+                                \Magento\Customer\Model\Session $customerSession,
+                                \Magento\Checkout\Model\Session $checkoutSession,
                                 Registry $coreRegistry,
                                 array $data = []) {
-        parent::__construct($context);
+
+        parent::__construct($context, $data);
         $this->_coreRegistry = $coreRegistry;
+        $this->_catalogSession = $catalogSession;
+        $this->_customerSession = $customerSession;
+        $this->_checkoutSession = $checkoutSession;
     }
 
     public function _prepareLayout() {
@@ -21,4 +31,17 @@ class Hello extends Template
         $this->assign("trungtq", $trungtq);
         parent::_prepareLayout();
     }
+
+    public function getCatalogSession() {
+        return $this->_catalogSession;
+    }
+
+    public function getCustomerSession() {
+        return $this->_customerSession;
+    }
+
+    public function getCheckoutSession() {
+        return $this->_checkoutSession;
+    }
+
 }
