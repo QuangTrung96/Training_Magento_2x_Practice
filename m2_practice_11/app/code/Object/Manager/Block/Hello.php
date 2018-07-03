4419c13a1e3521de56a121f07d3ac48b4870528d
QuangTrung96 <trinhquangtrung646@gmail.com>
2018-07-03 18:07:59 +0700

    [practice_with_customizing_layout_illustration]

--- m2_practice_11/app/code/Object/Manager/Block/Hello.php
+++ m2_practice_11/app/code/Object/Manager/Block/Hello.php
@@ -3,29 +3,18 @@
 namespace Object\Manager\Block;
 
 use Magento\Framework\View\Element\Template;
-use Magento\Catalog\Model\ProductRepository;
+
 class Hello extends Template
 {
     protected $_productRepository;
 
     public function __construct(Template\Context $context,
-                                ProductRepository $productRepository,
                                 array $data = []) {
-
         parent::__construct($context, $data);
-        $this->_productRepository = $productRepository;
     }
 
     public function _prepareLayout() {
         parent::_prepareLayout();
-    }
-
-    public function getProductById($id) {
-        return $this->_productRepository->getById($id);
-    }
-  
-    public function getProductBySku($sku) {
-        return $this->_productRepository->get($sku);
     }
 
 }
