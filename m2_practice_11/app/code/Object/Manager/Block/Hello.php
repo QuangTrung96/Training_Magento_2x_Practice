1a5edb20d4dd8be249737f73ae878135885f3e18
QuangTrung96 <trinhquangtrung646@gmail.com>
2018-07-04 08:49:01 +0700

    [practice_with_using_repository_class]

--- m2_practice_11/app/code/Object/Manager/Block/Hello.php
+++ m2_practice_11/app/code/Object/Manager/Block/Hello.php
@@ -3,18 +3,36 @@
 namespace Object\Manager\Block;
 
 use Magento\Framework\View\Element\Template;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Api\Data\ProductInterfaceFactory;
 
 class Hello extends Template
 {
+    private $productFactory;
+    private $productRepository;
     protected $_productRepository;
-
+    
     public function __construct(Template\Context $context,
+    							ProductInterfaceFactory $productFactory,
+        						ProductRepositoryInterface $productRepository,
                                 array $data = []) {
         parent::__construct($context, $data);
+        $this->productFactory = $productFactory;
+        $this->productRepository = $productRepository;
     }
 
     public function _prepareLayout() {
         parent::_prepareLayout();
+    }
+
+    public function loadMyProduct($sku) {
+    	return $this->productRepository->get($sku);
+    }
+
+    public function createProduct() {
+    	$product = $this->productFactory->create();
+    	$product->setSku('MyNewProduct');
+    	$this->productRepository->save($product);
     }
 
 }
